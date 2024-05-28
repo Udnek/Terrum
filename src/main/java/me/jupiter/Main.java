1fa1122c9c263423863d8a67015bdb7cca83197c
JUP1TER <stevevets1000000@gmail.com>
2024-05-28 17:12:47 +0300

    Started working on net generator. Implemented image reading feature, so you can literally draw simulation map

--- src/main/java/me/jupiter/Main.java
+++ src/main/java/me/jupiter/Main.java
@@ -4,8 +4,7 @@ import me.jupiter.image_reader.NetMapReader;
 
 public class Main {
     public static void main(String[] args) {
-        NetMapReader test = new NetMapReader();
-        test.readNetMap("C:/Coding/images/test_highNetMap.png");
-        System.out.println(test.getWidth());
+        Net net = new Net();
+        net.initiateNet();
     }
 }
--- src/main/java/me/jupiter/Net.java
+++ src/main/java/me/jupiter/Net.java
@@ -2,6 +2,7 @@ package me.jupiter;
 
 import me.jupiter.image_reader.NetMapReader;
 import me.jupiter.object.NetDynamicVertex;
+import me.jupiter.object.NetStaticVertex;
 import me.jupiter.object.NetVertex;
 import me.jupiter.object.NetVoidVertex;
 import org.realityforge.vecmath.Vector3d;
@@ -9,21 +10,25 @@ import org.realityforge.vecmath.Vector3d;
 public class Net {
     public final int sizeX;
     public final int sizeZ;
+    private final String imagePath;
     private final float springStiffness;
     private final float springRelaxedLength;
+    public NetVertex[][] netMap;
     Net()
     {
         this.sizeX = 4;
         this.sizeZ = 4;
         this.springStiffness = 1;
         this.springRelaxedLength = 1;
+        this.imagePath = "C:/Coding/images/test_netmap.png";
     }
-    Net(int sizeX, int sizeZ, float springStiffness, float springRelaxedLength)
+    Net(int sizeX, int sizeZ, float springStiffness, float springRelaxedLength, String imagePath)
     {
         this.sizeX = sizeX;
         this.sizeZ = sizeZ;
         this.springStiffness = springStiffness;
         this.springRelaxedLength = springRelaxedLength;
+        this.imagePath = imagePath;
     }
 
     public double calculateDistance(Vector3d point1, Vector3d point2) {
@@ -35,14 +40,16 @@ public class Net {
 
     public void initiateNet() {
         NetMapReader reader = new NetMapReader();
-        reader.readNetMap("C:/Coding/images/test_netmap.png");
-        NetVertex[][] net = new NetVertex[reader.getWidth()][reader.getHeight()];
+        reader.readNetMap(imagePath);
+        netMap = new NetVertex[reader.getWidth()][reader.getHeight()];
         for (int i = 0; i < reader.getWidth(); i++) {
             for (int j = 0; j < reader.getHeight(); j++) {
                 if (reader.getColor(i, j).getBlue() == 255){
-                    net[i][j] = new NetDynamicVertex(new Vector3d(i, 0, j));
+                    netMap[i][j] = new NetDynamicVertex(new Vector3d(i, 0, j));
                 } else if (reader.getColor(i, j).getRed() == 255) {
-                    net[i][j] = new NetVoidVertex(new Vector3d(i, 0, j));
+                    netMap[i][j] = new NetVoidVertex(new Vector3d(i, 0, j));
+                } else {
+                    netMap[i][j] = new NetStaticVertex(new Vector3d(i, 0, j));
                 }
             }
         }
