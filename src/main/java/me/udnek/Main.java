a80bed4e4038d33921e47bf05bf2bdcc4511026b
Udnek <gleb.dudorov@gmail.com>
2024-06-04 12:22:25 +0300

    upgraded SmartPolygonHolder

--- src/main/java/me/udnek/Main.java
+++ src/main/java/me/udnek/Main.java
@@ -16,9 +16,9 @@ public class Main{
                 0.01,
                 0.5,
                 "small_frame.png");
-        scene.setInitialDeviation(2, 2, 4);*/
+        scene.setInitialDeviation(2, 2, 2, 2, 2);*/
         Settings settings;
-        settings = Settings.noRecording(2, 12, Settings.PolygonHolderType.SMART);
+        settings = Settings.noRecording(2, 12, Settings.PolygonHolderType.DEFAULT);
         //settings = Settings.withRecording(512, 512, "test2", 12, Settings.PolygonHolderType.SMART);
         Scene scene = new LightTestScene();
         runWithScene(scene, settings);
