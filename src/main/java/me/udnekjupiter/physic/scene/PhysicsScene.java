42a8d5945acf8c5722068a6f4c04de1000b90657
JUP1TER <stevevets1000000@gmail.com>
2024-07-04 16:13:49 +0300

    minor changes

--- src/main/java/me/udnekjupiter/physic/scene/PhysicsScene.java
+++ src/main/java/me/udnekjupiter/physic/scene/PhysicsScene.java
@@ -1,7 +1,12 @@
 package me.udnekjupiter.physic.scene;
 
 import me.udnekjupiter.physic.net.CellularNet;
+import me.udnekjupiter.physic.object.MassEssence;
+import me.udnekjupiter.physic.object.RKMObject;
 import org.realityforge.vecmath.Vector3d;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhysicsScene implements PhysicScene {
 
@@ -18,5 +23,64 @@ public class PhysicsScene implements PhysicScene {
     public void setInitialDeviation(int x, int z, double xNew, double yNew, double zNew) {
         if (net.getVertex(x, z) == null) return;
         net.getVertex(x, z).setPosition(new Vector3d(xNew, yNew, zNew));
+    }
+
+    public List<RKMObject> RKMObjects;
+
+    public void initializeObjectHandler(){
+        RKMObjects = new ArrayList<>();
+    }
+    public void addMassEssence(Vector3d position){
+        RKMObjects.add(new MassEssence(position));
+    }
+
+    public void addObject(RKMObject object){
+        RKMObjects.add(object);
+    }
+
+    public void updateObjectsCoefficients() {
+        for (RKMObject object : RKMObjects) {
+            object.calculateCoefficient1();
+        }
+        for (RKMObject object : RKMObjects) {
+            object.updateRKMPhaseVector1();
+        }
+        for (RKMObject object : RKMObjects) {
+            object.calculateCoefficient2();
+        }
+        for (RKMObject object : RKMObjects) {
+            object.updateRKMPhaseVector2();
+        }
+        for (RKMObject object : RKMObjects) {
+            object.calculateCoefficient3();
+        }
+        for (RKMObject object : RKMObjects) {
+            object.updateRKMPhaseVector3();
+        }
+        for (RKMObject object : RKMObjects) {
+            object.calculateCoefficient4();
+        }
+    }
+    public void updateObjectsPositionDifferentials(){
+        for (RKMObject object : RKMObjects) {
+            object.RKMCalculatePositionDifferential();
+        }
+    }
+    public void updateObjectsPositions(){
+        for (RKMObject object : RKMObjects) {
+            object.updatePosition();
+        }
+    }
+    public void syncObjectsRKMPhaseVectors(){
+        for (RKMObject object : RKMObjects) {
+            object.setCurrentRKMPhaseVector(new Vector3d[]{object.getPosition(), object.getVelocity()});
+        }
+    }
+
+    public void updateObjects(){
+        syncObjectsRKMPhaseVectors();
+        updateObjectsCoefficients();
+        updateObjectsPositionDifferentials();
+        updateObjectsPositions();
     }
 }
