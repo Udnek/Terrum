f5b3b7e9fa7a313df213f4a181f129aaa7e2e24b
Udnek <gleb.dudorov@gmail.com>
2024-07-05 09:46:59 +0300

    iterator

--- src/main/java/me/udnekjupiter/util/Utils.java
+++ src/main/java/me/udnekjupiter/util/Utils.java
@@ -1,12 +1,21 @@
 package me.udnekjupiter.util;
 
 import java.text.NumberFormat;
+import java.util.function.Consumer;
 
 public class Utils {
     public static String roundToPrecision(double number, int precision){
         NumberFormat numberFormat = NumberFormat.getInstance();
         numberFormat.setMinimumFractionDigits(precision);
         return numberFormat.format(number);
+    }
+
+    public static <T> void iterate(T[][] array, Consumer<T> consumer){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                consumer.accept(array[i][j]);
+            }
+        }
     }
 
 }
