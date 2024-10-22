c037d56f7bdd475a698c89e45ece3e18517a3012
Udnek <gleb.dudorov@gmail.com>
2024-10-22 14:20:27 +0300

    PhysicEngine

--- src/main/java/me/udnekjupiter/physic/engine/PhysicEngine.java
+++ src/main/java/me/udnekjupiter/physic/engine/PhysicEngine.java
@@ -1,11 +1,15 @@
 package me.udnekjupiter.physic.engine;
 
+import me.udnekjupiter.physic.EnvironmentSettings;
+import me.udnekjupiter.physic.object.PhysicObject;
 import me.udnekjupiter.physic.scene.PhysicScene;
 import me.udnekjupiter.util.Initializable;
 import me.udnekjupiter.util.Resettable;
 import me.udnekjupiter.util.Tickable;
 import org.jetbrains.annotations.NotNull;
 
-public interface PhysicEngine<PScene extends PhysicScene<?>> extends Initializable, Tickable, Resettable {
-    @NotNull PScene getScene();
+public interface PhysicEngine<ObjectType extends PhysicObject<?>> extends Initializable, Tickable, Resettable {
+    <Scene extends PhysicScene<ObjectType>> @NotNull Scene getScene();
+    @NotNull EnvironmentSettings getSettings();
+    void addObject(@NotNull ObjectType object);
 }
