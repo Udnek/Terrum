f515fb6254ab39e139a88fa82041c35a504139f9
JUP1TER-c1 <stevevets1000000@gmail.com>
2024-10-23 00:54:38 +0300

    springs yipeeee

--- src/main/java/me/udnekjupiter/physic/object/vertex/NetDynamicVertex.java
+++ src/main/java/me/udnekjupiter/physic/object/vertex/NetDynamicVertex.java
@@ -4,6 +4,7 @@ package me.udnekjupiter.physic.object.vertex;
 import me.udnekjupiter.Main;
 import me.udnekjupiter.app.StandartApplication;
 import me.udnekjupiter.physic.EnvironmentSettings;
+import me.udnekjupiter.physic.engine.PhysicEngine3d;
 import me.udnekjupiter.util.VectorUtils;
 import org.jetbrains.annotations.NotNull;
 import org.realityforge.vecmath.Vector3d;
@@ -20,14 +21,7 @@ public class NetDynamicVertex extends NetVertex {
 
     @Override
     public void calculateForces(@NotNull Vector3d position) {
-        container.appliedForce.mul(0);
-        for (NetVertex neighbor : neighbors) {
-            Vector3d normalizedDirection = VectorUtils.getNormalizedDirection(position, neighbor.getPosition());
-            double distanceToNeighbour = VectorUtils.distance(position, neighbor.getPosition());
-            double distanceDifferential = distanceToNeighbour - springRelaxedLength;
-            double elasticForce = springStiffness * distanceDifferential;
-            container.appliedForce.add(normalizedDirection.mul(elasticForce));
-        }
+        container.appliedForce.y += PhysicEngine3d.GRAVITATIONAL_ACCELERATION * container.mass;
         container.appliedForce.add(getCollisionForce());
     }
 }
